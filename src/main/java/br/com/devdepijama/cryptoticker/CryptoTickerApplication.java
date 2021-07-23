6b5a8376103170b841302082a932f130acb93c0b
Hugo Pinto <devdepijama@gmail.com>
2021-07-23 19:08:31 +0100

    Initial commit

---
+++ src/main/java/br/com/devdepijama/cryptoticker/CryptoTickerApplication.java
@@ -0,0 +1,13 @@
+package br.com.devdepijama.cryptoticker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoTickerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoTickerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/devdepijama/cryptoticker/CryptoTickerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devdepijama.cryptoticker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoTickerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
